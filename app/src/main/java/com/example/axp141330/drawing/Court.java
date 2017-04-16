2c5c394541c775a1c49ac44fd47f03d18127a244
alanpadillachua <axp141330@utdallas.edu>
2017-04-15 20:10:53 -0500

    initial commit

---
+++ app/src/main/java/com/example/axp141330/drawing/Court.java
@@ -0,0 +1,34 @@
+package com.example.axp141330.drawing;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by laptop on 4/6/2017.
+ */
+
+public class Court extends View {
+    private ShapeDrawable rectangle;
+    public Court(Context context, AttributeSet attr){
+        super(context,attr);
+
+
+        int width = context.getResources().getDisplayMetrics().widthPixels;
+        int height = context.getResources().getDisplayMetrics().widthPixels;
+        int x = height/2;
+        int y = 0;
+        rectangle = new ShapeDrawable(new RectShape());
+        rectangle.getPaint().setColor(0xff74AC23);
+        rectangle.setBounds(x, y, x + width, y + height);
+    }
+
+
+    @Override
+    public void onDraw(Canvas canvas){
+        rectangle.draw(canvas);
+    }
+}
