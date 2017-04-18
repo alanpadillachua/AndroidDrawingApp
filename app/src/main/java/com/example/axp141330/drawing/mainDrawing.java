08ce5e1744d06a8b97034d3aa4f27c1e242acc08
alanpadillachua <axp141330@utdallas.edu>
2017-04-18 17:59:50 -0500

    purge the branch

--- app/src/main/java/com/example/axp141330/drawing/mainDrawing.java
+++ app/src/main/java/com/example/axp141330/drawing/mainDrawing.java
@@ -3,6 +3,7 @@ package com.example.axp141330.drawing;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -14,7 +15,8 @@ public class mainDrawing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_drawing);
-
+        Toolbar toolbar = (Toolbar)findViewById(R.id.mainToolBar);
+        setSupportActionBar(toolbar);
 
     }
 }
