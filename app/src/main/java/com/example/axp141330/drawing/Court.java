49d9140aa7621595fd95531947ba05c8d9d97677
alanpadillachua <axp141330@utdallas.edu>
2017-04-17 23:26:23 -0500

    buttons added with slider and buttons gotten from resources

--- app/src/main/java/com/example/axp141330/drawing/Court.java
+++ app/src/main/java/com/example/axp141330/drawing/Court.java
@@ -9,36 +9,42 @@ import android.graphics.drawable.shapes.RectShape;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.RadioButton;
 
 /**
  * Created by Alan Padilla on 4/6/2017.
  */
 
-public class Court extends View implements View.OnTouchListener{
+public class Court extends View {
     private ShapeDrawable rectangle;
     private ShapeDrawable circle;
+    private RadioButton orange;
+    private RadioButton purple;
+    private RadioButton pink;
+    private RadioButton green;
+    private int buttonColor;
     public Court(Context context, AttributeSet attr){
         super(context,attr); // call super
         int width = context.getResources().getDisplayMetrics().widthPixels; // get width of screen
         int height = context.getResources().getDisplayMetrics().heightPixels;
         int x = 0; // set x start of screen 0
-        int y = height-width; // y will be the height minus width
+        int y = 0; // y will be the height - width
         rectangle = new ShapeDrawable(new RectShape()); // initialize a rectangle
-        rectangle.getPaint().setColor(Color.GRAY); // set the color
+        rectangle.getPaint().setColor(Color.BLACK); // set the color
         rectangle.setBounds(x, y, width, height); // make it be on bottom half of screen
 
-        circle = new ShapeDrawable(new OvalShape());
-        circle.getPaint().setColor(Color.BLUE);
+        orange = (RadioButton)findViewById(R.id.OrangeButton); // get orange button
+        purple = (RadioButton)findViewById(R.id.PurpleButton); // get purple button
+        green = (RadioButton)findViewById(R.id.GreenButton); // get green button
+        pink = (RadioButton)findViewById(R.id.PinkButton); // get pink button
+
     }
-    @Override
-    public boolean onTouch(View view, MotionEvent event){
-        return true;
-    }
+
 
 
     @Override
     public void onDraw(Canvas canvas){
         rectangle.draw(canvas); // call rectangle to draw
-        circle.draw(canvas); // draw circle
+
     }
 }
