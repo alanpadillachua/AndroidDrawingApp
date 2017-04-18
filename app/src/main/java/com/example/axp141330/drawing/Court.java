5e47f815a67f6ebf3db883185477711273e341e7
alanpadillachua <axp141330@utdallas.edu>
2017-04-18 00:47:43 -0500

    drawing is hard

--- app/src/main/java/com/example/axp141330/drawing/Court.java
+++ app/src/main/java/com/example/axp141330/drawing/Court.java
@@ -6,10 +6,14 @@ import android.graphics.Color;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.graphics.drawable.shapes.RectShape;
+import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 
 /**
  * Created by Alan Padilla on 4/6/2017.
@@ -23,11 +27,13 @@ public class Court extends View {
     private RadioButton pink;
     private RadioButton green;
     private int buttonColor;
+    private RadioGroup colorChoiceButton;
     private final int OrangeColor = Color.parseColor("#ff9400");
     private final int PurpleColor = Color.parseColor("#a100c1");
     private final int PinkColor = Color.parseColor("#ff0098");
+    private final int greenColor = Color.parseColor("#05aa00");
 
-    public Court(Context context, AttributeSet attr){
+    public Court(final Context context, AttributeSet attr){
         super(context,attr); // call super
         int width = context.getResources().getDisplayMetrics().widthPixels; // get width of screen
         int height = context.getResources().getDisplayMetrics().heightPixels;
@@ -36,11 +42,13 @@ public class Court extends View {
         rectangle = new ShapeDrawable(new RectShape()); // initialize a rectangle
         rectangle.getPaint().setColor(Color.BLACK); // set the color
         rectangle.setBounds(x, y, width, height); // make it be on bottom half of screen
+        circle = new ShapeDrawable(new OvalShape());
+        circle.setIntrinsicWidth(width);
 
-        orange = (RadioButton)findViewById(R.id.OrangeButton); // get orange button
-        purple = (RadioButton)findViewById(R.id.PurpleButton); // get purple button
-        green = (RadioButton)findViewById(R.id.GreenButton); // get green button
-        pink = (RadioButton)findViewById(R.id.PinkButton); // get pink button
+
+
+
+
 
     }
 
@@ -49,6 +57,7 @@ public class Court extends View {
     @Override
     public void onDraw(Canvas canvas){
         rectangle.draw(canvas); // call rectangle to draw
+        //circle.draw(canvas);
 
     }
 }
